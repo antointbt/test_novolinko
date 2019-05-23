a0c093e71e482c404ddc09e7b553c9ac10cfcb29
Antoine <antoine.boudet@epitech.eu>
2019-05-23 03:23:50 -0700

    improve admin page

--- NovolinkoTest-client/src/app/admin/admin.component.ts
+++ NovolinkoTest-client/src/app/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+  disabledSubmitButton: boolean = true;
+  optionsSelect: Array<any>;
+  checkPwd: boolean = true;
 
   ngOnInit() {
   }
+
+  @HostListener('input') oninput() {
+
+    if (this.contactForm.valid) {
+      this.disabledSubmitButton = false;
+      }
+    }
+  
+    constructor(
+      private router: Router,
+      private fb: FormBuilder
+      ) {
+  
+      this.contactForm = fb.group({
+        'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
+        'contactFormPassword': ['', Validators.required]
+      });
+    }
+  
+    onSubmit() {
+      if (this.contactForm.value.contactFormEmail == "antoine@gmail.com" && this.contactForm.value.contactFormPassword == "root")
+        this.router.navigate(['/chooseAdminOption']);
+      else
+        this.checkPwd = false;
+    }
 
 }

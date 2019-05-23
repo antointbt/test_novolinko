53ddb269538d175f8f8bfb7d3737d26cfbf7ce2d
Antoine <antoine.boudet@epitech.eu>
2019-05-23 03:16:53 -0700

    add route for admin component

--- NovolinkoTest-client/src/app/app.module.ts
+++ NovolinkoTest-client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'admin', component: AdminComponent },
   { path: 'tickets', component: TicketListComponent },
   { path: 'createTicket', component: TicketFormComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
